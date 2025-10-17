53440d9676971bbe7b2b1d9605a1ca8280249ec5
Vladli Oleksuk <vladoleksuk0@gmail.com>
2025-10-17 16:38:21 +0200

    refactor: sistemati gli spazi nel codice

--- 01/main.py
+++ 01/main.py
@@ -1,18 +1,17 @@
 def calculator():
-
     print("Calcolatrice semplice")
 
     num1 = float(input("Inserisci il primo numero"))
     operazione = input("Scgeli operazione:(+, -, /, *).")
     num2 = float(input("Inserisci il secondo numero"))
 
-    if operazione == '+':
+    if operazione == "+":
         print("Resultato:", num1 + num2)
-    elif operazione == '-':
+    elif operazione == "-":
         print("Resultato:", num1 - num2)
-    elif operazione == '*':
+    elif operazione == "*":
         print("Resultato:", num1 * num2)
-    elif operazione == '/':
+    elif operazione == "/":
         if num2 != 0:
             print("Resultato:", num1 / num2)
         else:
